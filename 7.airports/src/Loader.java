5f30d0b5e6c80223c3302399a811c1e384b06648
sofia_f <lx.sonik.xl@gmail.com>
2019-06-28 17:50:35 +0300

    pops

--- 7.airports/src/Loader.java
+++ 7.airports/src/Loader.java
@@ -8,7 +8,6 @@ public class Loader {
 
 
         Airport airport = Airport.getInstance();
-        List<Terminal> terms = airport.getTerminals();
 
 
         Date now = new Date();
@@ -17,7 +16,7 @@ public class Loader {
 
         ArrayList<Flight> flights = airport.getAllFlights();
         flights.stream()
-                .filter(f -> f.getType() == Flight.Type.DEPARTURE && f.getDate().before(inTwoHours) && f.getDate().after(now))  //вылетающие в ближайшие 2 часа
+                .filter(f -> f.getType() == Flight.Type.DEPARTURE && f.getDate().before(inTwoHours) && f.getDate().after(now))  //вылетающие (!) в ближайшие 2 часа
                 .sorted(Comparator.comparing(Flight::getDate))
                 .forEach(f -> System.out.println(Flight.HOUR_FORMAT.format(f.getDate()) + " " + f.getAircraft().getModel()));  //распечатать время вылета и модели самолётов
 
